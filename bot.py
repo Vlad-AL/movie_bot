a11adeef55eca66cd11e4fa85399dd06c01697d7
Vlad-AL <vladislav.al.555@gmail.com>
2026-03-29 15:21:37 +0300

    изменение

--- bot.py
+++ bot.py
@@ -10,14 +10,13 @@ from aiogram.client.session.aiohttp import AiohttpSession
 TOKEN = "8425155912:AAFT4AIwrRphrV8g4IenxwxIL2wSRN95uKA"
 
 # Твой немецкий SOCKS5 прокси
-PROXY_URL = "socks5://r4nygK:A****8@196.**.**.69:8000"
+PROXY_URL = "socks5://r4nygK:A5dXE8@196.19.10.69:8000"
 
 session = AiohttpSession(proxy=PROXY_URL, timeout=60)
 
 bot = Bot(token=TOKEN, session=session)
 dp = Dispatcher()
 
-PROXY_URL = "socks5://r4nygK:A5dXE8@196.19.10.69:8000"
 # db = sqlite3.connect("users.db", check_same_thread=False)
 # cursor = db.cursor()
 
