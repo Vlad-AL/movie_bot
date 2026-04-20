ac32a876dfa085d75e2a7abf8ef9d38bde51a79f
Vlad-AL <vladislav.al.555@gmail.com>
2026-04-20 22:52:15 +0300

    изменение

--- bot.py
+++ bot.py
@@ -4319,7 +4319,7 @@ async def check_movie_callback(callback: types.CallbackQuery):
         )
         return
 
-    hashtags = " ".join(f"#{g.replace(' ', '_')}" for g in movie.get("genres", []))
+    hashtags = " ".join(f"#{g.replace(' ', '_')}@kinonawe4er" for g in movie.get('genres', []))
 
     await callback.message.answer_video(
         video=movie["video"],
@@ -4363,7 +4363,7 @@ def serial_start_keyboard(code: str):
 
 async def send_serial_card(message: types.Message, code: str):
     serial = series[code]
-    hashtags = " ".join(f"#{g.replace(' ', '_')}" for g in serial.get("genres", []))
+    hashtags = " ".join(f"#{g.replace(' ', '_')}@kinonawe4er" for g in movie.get('genres', []))
     text = (
         f"<b>⭐️ «{serial['title']}», {serial['year']}, ({serial['episode_counter']})</b>\n\n"
         f"<i>{serial['description']}</i>\n\n"
@@ -4682,7 +4682,7 @@ async def handle_message(message: types.Message):
                 )
                 return
 
-            hashtags = " ".join(f"#{g.replace(' ', '_')}" for g in movie.get("genres", []))
+            hashtags = " ".join(f"#{g.replace(' ', '_')}@kinonawe4er" for g in movie.get('genres', []))
 
             await message.answer_video(
                 video=movie["video"],
