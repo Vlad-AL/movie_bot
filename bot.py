be7b950240b5febcc67a9d3069ac9eafb155375a
Vlad-AL <vladislav.al.555@gmail.com>
2026-05-03 15:36:34 +0300

    изменение

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.filters import Command
 import asyncio
-import datetime
+from datetime import datetime
 from aiogram.client.session.aiohttp import AiohttpSession
 from dotenv import load_dotenv
 import os
