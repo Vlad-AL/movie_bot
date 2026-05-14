7fa3bbe41f33e92de99e272f52e3b5e00713153d
Vlad-AL <vladislav.al.555@gmail.com>
2026-05-14 21:56:03 +0300

    изменение

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ TOKEN = os.getenv("TOKEN")
 if not TOKEN:
     raise ValueError("Токен не найден в .env файле!")
 
-PROXY_URL = "http://uapkuolo:00j38yrboe49@31.59.20.176:6754"
+PROXY_URL = "http://uapkuolo:00j38yrboe49@142.111.48.253:7030"
 
 
 session = AiohttpSession(
